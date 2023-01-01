1fc1a54afc2b8d4efd1cf7def3ce5a718bdee48b
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2023-01-01 21:58:06 +0300

    Initial Commit

---
+++ Bendera/BenderaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenderaApp.swift
+//  Bendera
+//
+//  Created by Leonard Mutugi on 01/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BenderaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
