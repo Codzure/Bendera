76f3e988111259a1263009e83dff9e6558c3a06c
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2023-01-02 22:47:22 +0300

    Learning --building blocks

--- Bendera/ContentView.swift
+++ Bendera/ContentView.swift
@@ -7,46 +7,101 @@
 
 import SwiftUI
 
+/// <#Description#>
 struct ContentView: View {
-    @State private var time = 0
+    @State private var showAlert = false
     var body: some View {
-        ZStack{
-            VStack(spacing: 0){
-                Color.red
-                Color.blue
-            }
-            
-//            LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
-            
-//            LinearGradient(gradient: Gradient(stops: [
-//                    Gradient.Stop(color: .white, location: 0.45),
-//                    Gradient.Stop(color: .black, location: 0.55),
-//                ]), startPoint: .top, endPoint: .bottom)
-            
-//            LinearGradient(gradient: Gradient(stops: [
-//                    .init(color: .white, location: 0.45),
-//                    .init(color: .black, location: 0.55),
-//                ]), startPoint: .top, endPoint: .bottom)
-
-//            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
-            
-//            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
-
-
-            Text("Choose your flag")
-                .foregroundColor(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-            
-        }
-        .ignoresSafeArea()
-
         
-        }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+       
     }
 }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    // MARK: 3 types of SwiftUI gradients
+    
+    //            LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+    
+    //            LinearGradient(gradient: Gradient(stops: [
+    //                    Gradient.Stop(color: .white, location: 0.45),
+    //                    Gradient.Stop(color: .black, location: 0.55),
+    //                ]), startPoint: .top, endPoint: .bottom)
+    
+    //            LinearGradient(gradient: Gradient(stops: [
+    //                    .init(color: .white, location: 0.45),
+    //                    .init(color: .black, location: 0.55),
+    //                ]), startPoint: .top, endPoint: .bottom)
+    
+    //            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+    
+    //            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+    
+    
+    
+    //Button {
+    //    print("Button was tapped")
+    //} label: {
+    //    Text("Tap me!")
+    //        .padding()
+    //        .foregroundColor(.white)
+    //        .background(.orange)
+    //        .buttonStyle(.borderedProminent)
+    //
+    //}
+    
+    
+    //            Button("Delete selection",role: .destructive, action: executeDelete)
+    //                .buttonStyle(.borderedProminent)
+    //                .tint(.mint)
+    //
+    //            Text("Choose your flag")
+    //                .foregroundColor(.secondary)
+    //                .padding(50)
+    //                .background(.ultraThinMaterial)
+    
+    
+    
+//    ZStack{
+//        VStack(spacing: 0){
+//            Color.red
+//            Color.blue
+//        }
+//
+//
+//
+//        Button {
+//            print("Edit button was tapped")
+//        } label: {
+//            Label("Edit", systemImage: "pencil")
+//        }
+//
+//    }
+//    .ignoresSafeArea()
+//
+//
+//    }
+//
+//func executeDelete(){
+//    print("Deleting ...")
+//}
+
+//    .alert("Important message", isPresented: $showAlert){
+//        Button("Ok"){}
+//    }
+//
+//    .alert("Important message", isPresented: $showAlert) {
+//        Button("Delete", role: .destructive) { }
+//        Button("Cancel", role: .cancel) { }
+//    }
+
+//Button("Show Alert"){
+//    showAlert = true
+//}
+//.alert("Important message", isPresented: $showAlert) {
+//    Button("OK", role: .cancel) { }
+//} message: {
+//    Text("Please read this.")
+//}
